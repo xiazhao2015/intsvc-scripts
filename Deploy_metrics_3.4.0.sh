12a75642b7d913d2de29c0d99440b593398b84df
Xia Zhao <xiazhao@redhat.com>
2016-12-05 18:02:56 +0800

    Update Deploy_metrics_3.4.0.sh

--- Deploy_metrics_3.4.0.sh
+++ Deploy_metrics_3.4.0.sh
@@ -1,15 +1,15 @@
     #wget https://raw.githubusercontent.com/openshift/origin-metrics/enterprise/metrics.yaml
     #curl -O metrics.yaml https://raw.githubusercontent.com/openshift/origin-metrics/enterprise/metrics.yaml
      
-    PREFIX=registry.ops.openshift.com/openshift3/
+PREFIX=registry.ops.openshift.com/openshift3/
     #PREFIX=brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888/openshift3/
-    SUBDOMAIN=`grep subd /etc/origin/master/master-config.yaml | awk '{ print $2 }' | sed 's:^.\(.*\).$:\1:'`
-    MASTERURL=`grep masterURL /etc/origin/master/master-config.yaml | awk '{ print $2 }'`
+SUBDOMAIN=`grep subd /etc/origin/master/master-config.yaml | awk '{ print $2 }' | sed 's:^.\(.*\).$:\1:'`
+MASTERURL=`grep masterURL /etc/origin/master/master-config.yaml | awk '{ print $2 }'`
      
      
      
      
-    oc project openshift-infra
+oc project openshift-infra
      
     #oc create -f - <<API
     #apiVersion: v1
@@ -20,19 +20,19 @@
     #- name: metrics-deployer
     #API
     
-    oc create serviceaccount metrics-deployer
+oc create serviceaccount metrics-deployer
      
-    oadm policy add-role-to-user edit system:serviceaccount:openshift-infra:metrics-deployer
+oadm policy add-role-to-user edit system:serviceaccount:openshift-infra:metrics-deployer
      
-    oc secrets new metrics-deployer nothing=/dev/null
-    oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:openshift-infra:heapster
+oc secrets new metrics-deployer nothing=/dev/null
+oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:openshift-infra:heapster
 
     # Added in 3.4.0
-    oadm policy add-role-to-user view system:serviceaccount:openshift-infra:hawkular -n openshift-infra
-    oadm policy add-cluster-role-to-user cluster-admin system:serviceaccount:openshift-infra:metrics-deployer
+oadm policy add-role-to-user view system:serviceaccount:openshift-infra:hawkular -n openshift-infra
+oadm policy add-cluster-role-to-user cluster-admin system:serviceaccount:openshift-infra:metrics-deployer
      
     #Modified in 3.4.0
-    oc new-app -f metrics.yaml --as=system:serviceaccount:openshift-infra:metrics-deployer \
+oc new-app -f metrics.yaml --as=system:serviceaccount:openshift-infra:metrics-deployer \
     -p IMAGE_PREFIX=$PREFIX \
     -p IMAGE_VERSION=3.4.0 \
     -p MASTER_URL=$MASTERURL \
